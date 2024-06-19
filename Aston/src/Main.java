959a55d414c358e08a4b75e40d972302e36602e6
Elena-Rubanova <rubanovaelena91@gmail.com>
2024-06-19 17:25:07 +0300

    ветка Lesson_6, пакет lesson6 и класс Lesson6

--- Aston/src/Main.java
+++ Aston/src/Main.java
@@ -1,5 +1,5 @@
-import lesson6.Employee;
-
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
 
