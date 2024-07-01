9547c2b3fa387a15def80e7d2556e62115bf9b26
Elena-Rubanova <rubanovaelena91@gmail.com>
2024-07-01 18:22:13 +0300

    ветка Lesson_9, пакет lesson9. Использование коллекций

--- Aston/src/Main.java
+++ Aston/src/Main.java
@@ -1,15 +1,45 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import lesson9.Phonebook;
+
+import java.util.*;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        //задание1
+        List<String> words = Arrays.asList("Собака", "Кот", "Жираф", "Осел", "Верблюд",
+                "Кот", "Лошадь", "Лошадь", "Олень", "Жираф",
+                "Собака", "Верблюд", "Жираф", "Лев", "Тигр",
+                "Панда", "Баран", "Олень", "Волк", "Жираф");
+
+        Set<String> unique = new HashSet<>(words);
+
+        System.out.println("Первоначальный массив:" + "\n" + words + "\n");
+        System.out.println("Уникальные слова:" + "\n" + unique + "\n");
+        System.out.println("Слово встречается:");
+        for (String key : unique) {
+            System.out.println(key + ": " + Collections.frequency(words, key) + " раз(а)");
+        }
+
+        System.out.println();
+
+        //задание 2
+        Phonebook phonebook = new Phonebook();
+        phonebook.add("Иванов", "+375293399569");
+        phonebook.add("Петров", "+375444585263");
+        phonebook.add("Сидоров", "+375335865752");
+        phonebook.add("Иванова", "+375259995588");
+        phonebook.add("Иванов", "+375295588369");
+
+        String surname = "Петров";
+
+        List<String> phones = phonebook.get(surname);
+
+        if (phones.isEmpty()) {
+            System.out.println("Номер телефона по фамилии " + surname + " не найден.");
+        } else {
+            System.out.println("Номер(а) телефона(ов) по фамилии " + surname + ":");
+            phones.forEach(System.out::println);
         }
     }
 }
+
---
+++ Aston/src/lesson9/Phonebook.java
@@ -0,0 +1,28 @@
+package lesson9;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Phonebook {
+    private final Map<String, List<String>> phonebook = new HashMap<>();
+
+    public void add(String surname, String number) {
+        List<String> phones = phonebook.getOrDefault(surname, new ArrayList<>());
+        phones.add(number);
+        phonebook.put(surname, phones);
+    }
+
+    public List<String> get(String surname) {
+        return phonebook.getOrDefault(surname, new ArrayList<>());
+    }
+
+}
+
+
+
+
+
+
+
