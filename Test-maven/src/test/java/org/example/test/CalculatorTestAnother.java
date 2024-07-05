db86c1646700734e32896af15ae5f6c2f7021ddc
Elena-Rubanova <rubanovaelena91@gmail.com>
2024-07-05 21:26:32 +0300

    test commit

---
+++ Test-maven/src/test/java/org/example/test/CalculatorTestAnother.java
@@ -0,0 +1,12 @@
+package org.example.test;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CalculatorTestAnother {
+
+    @Test
+    void sum() {
+    }
+}
