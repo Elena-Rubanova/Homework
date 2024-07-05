7ece7658ec0bf4b62ab5032eb70c061cb4057836
Elena-Rubanova <rubanovaelena91@gmail.com>
2024-07-05 21:40:16 +0300

    test commit

---
+++ Test-maven/src/main/java/by/aston/lesson10/testNG/Factorial.java
@@ -0,0 +1,7 @@
+package by.aston.lesson10.testNG;
+
+public class Factorial {
+    public int calculateFactorial(int a) {
+        return 0;
+    }
+}
---
+++ Test-maven/src/test/java/by/aston/lesson10/testNG/FactorialTest.java
@@ -0,0 +1,12 @@
+package by.aston.lesson10.testNG;
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+public class FactorialTest {
+
+    @Test
+    public void testCalculateFactorial() {
+    }
+}
