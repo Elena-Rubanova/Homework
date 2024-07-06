6a6d67d5ecf1dd5a23e2a98b547df6a6f96efca8
Elena-Rubanova <rubanovaelena91@gmail.com>
2024-07-06 11:21:12 +0300

    Написала программу позволяющую вычислить факториал числа и юнит-тесты для этой программы

---
+++ Test-maven/src/main/java/Main.java
@@ -0,0 +1,9 @@
+import com.aston.lesson10.Factorial;
+
+public class Main {
+    public static void main(String[] args) {
+        Factorial factorial = new Factorial();
+
+    }
+}
+
---
+++ Test-maven/src/main/java/com/aston/lesson10/Factorial.java
@@ -0,0 +1,17 @@
+package com.aston.lesson10;
+
+import java.math.BigInteger;
+
+public class Factorial {
+    public static BigInteger factorial(int number) {
+        if (number < 0) throw new IllegalArgumentException("Число должно быть положительным");
+
+        else if (number <= 1) {
+            return BigInteger.valueOf(1);
+        } else {
+            return BigInteger.valueOf(number).multiply(factorial(number - 1));
+        }
+    }
+}
+
+
