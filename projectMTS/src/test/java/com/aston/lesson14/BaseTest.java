04d583d2270faacc128d40849ccc19e776f161e4
Elena-Rubanova <rubanovaelena91@gmail.com>
2024-07-25 17:01:17 +0300

    Написаны тесты для сайта МТС( проверка модуля оплаты)

---
+++ projectMTS/src/test/java/com/aston/lesson14/BaseTest.java
@@ -0,0 +1,39 @@
+package com.aston.lesson14;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+
+public abstract class BaseTest {
+    private WebDriver webDriver;
+    private PaymentModule paymentModule;
+    private final String URL = "https://www.mts.by/";
+    public PaymentModule getPaymentModule() {
+        return paymentModule;
+    }
+
+    public WebDriver initializeDriver(boolean headless) {
+        ChromeOptions options = new ChromeOptions();
+        if (headless) {
+            options.addArguments("--headless");
+        }
+        return new ChromeDriver(options);
+    }
+
+    @BeforeTest
+    protected void beforeTest() {
+        webDriver = initializeDriver(true);
+        webDriver.get(URL);
+        webDriver.manage().window().maximize();
+        paymentModule = new PaymentModule(webDriver);
+        paymentModule.acceptCookies();
+    }
+    @AfterTest
+    protected void afterTest() {
+        if(webDriver!=null) {
+            webDriver.quit();
+        }
+    }
+}
---
+++ projectMTS/src/test/java/com/aston/lesson14/IframeTest.java
@@ -0,0 +1,51 @@
+package com.aston.lesson14;
+
+import org.testng.Assert;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import java.util.List;
+
+public class IframeTest extends BaseTest {
+    private final String NUMBER = "297777777";
+    private final String PAYMENT = "100";
+    private final List<String> LOGO_LIST = List.of("mastercard", "visa", "belkart", "mir", "maestro");
+    private final List<String> TITLE_FIELD = List.of(PAYMENT + ".00 BYN", "Оплата: Услуги связи Номер:375" + NUMBER,
+            "Номер карты", "Срок действия", "Имя держателя (как на карте)", "CVC", "Оплатить " + PAYMENT + ".00 BYN");
+
+    @Test(description = "Проверка информации о логотипах в iFrame", dataProvider = "logosInfo")
+    public void checkLogoTest(String number, String payment, int amount, List<String> logoname) {
+
+        Iframe iFrame = getPaymentModule().sendParametersAndButtonClick(number, payment);
+
+        Assert.assertEquals(iFrame.getListLogosName().size(), amount,
+                "Ошибка в количестве логотипов");
+
+        iFrame.closeIFrame();
+    }
+    @DataProvider
+    public Object[][] logosInfo() {
+        return new Object[][]{
+                {NUMBER, PAYMENT, 5, LOGO_LIST}
+        };
+    }
+    @Test(description = "Проверка поля информации", dataProvider = "fieldInformation")
+    void checkFieldInformationTest(String number, String payment, List<String> expected) {
+
+        Iframe iFrame = getPaymentModule().sendParametersAndButtonClick(number, payment);
+
+        List<String> actualField = iFrame.getListPlaceholderName();
+
+        for (int i = 0; i < actualField.size(); i++) {
+            Assert.assertEquals(iFrame.getListPlaceholderName().get(i), expected.get(i),
+                    "Ошибка в названии поля");
+        }
+        iFrame.closeIFrame();
+    }
+    @DataProvider
+    public Object[][] fieldInformation() {
+        return new Object[][]{
+                {NUMBER, PAYMENT, TITLE_FIELD}
+        };
+    }
+}
